1f3c8a5c692b0d3dbe80be0abe5b50d4e84092eb
amiksha125 <amikshagajbhiye@gmail.com>
2025-06-18 02:05:15 +0530

    display search bar when search incon on navbar clicked

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 // import React from 'react'
 import {assets} from '../assets/assets'
 import { NavLink, Link } from 'react-router-dom'
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
 
 
 const Navbar = () => {
     // define a state, whenever click menu icon the visible state will be true
     const [visible, setVisible] = useState(false);
+
+    //use search state
+    const {setShowSearch} = useContext(ShopContext);
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
 
@@ -37,7 +41,7 @@ const Navbar = () => {
 
         <div className='flex items-center gap-6'> 
 
-            <img src= {assets.search_icon} className='w-5 cursor-pointer' alt="" />
+            <img onClick = {() => setShowSearch(true)} src= {assets.search_icon} className='w-5 cursor-pointer' alt="" />
 
             <div className='group relative'>
                 <img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" />
--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -10,7 +10,7 @@ const ShopContextProvider = (props) => {
     const delivery_fee = 10;
     //search icon 
     const [search, setSearch] = useState('');
-    const [showSearch, setShowSearch] = useState(true);
+    const [showSearch, setShowSearch] = useState(false);
 
     const value = {
          products, currency, delivery_fee,
