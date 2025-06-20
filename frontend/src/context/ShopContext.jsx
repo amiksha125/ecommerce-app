340054a78468f06c806f90ec5ca215fbfa9c35bd
amiksha125 <amikshagajbhiye@gmail.com>
2025-06-20 23:30:22 +0530

    create a cartItem state variable, addTocart func, when button clicked add the product with size and increase the quantity

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import { products } from '../assets/assets';
 
 export const ShopContext = createContext();
@@ -11,10 +11,36 @@ const ShopContextProvider = (props) => {
     //search icon 
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
+    const [cartItems, setCartItems] = useState({});
+
+    const addToCart = async(itemId, size) => {
+     //create a copy of cart item object
+     let cartData = structuredClone(cartItems);
+
+     if(cartData[itemId]){
+          if(cartData[itemId][size]){
+               cartData[itemId][size] += 1;
+          } else {
+               cartData[itemId][size] = 1;
+          }
+     }
+     else {
+          cartData[itemId] = {};
+          cartData[itemId][size] = 1;
+     }
+
+       setCartItems(cartData);
+
+    }
+
+    useEffect(() => {
+         console.log(cartItems);
+    }, [cartItems])//whenever this, [cartItems] get modified useEfffect will execute
 
     const value = {
          products, currency, delivery_fee,
-         search, setSearch, showSearch, setShowSearch
+         search, setSearch, showSearch, setShowSearch,
+         cartItems, addToCart
     }
 
     return (
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -9,7 +9,7 @@ const Product = () => {
   //check royute , we are getting productid fron url
   const { productId } = useParams();
   // console.log(productId)
-  const { products, currency } = useContext(ShopContext);
+  const { products, currency, addToCart} = useContext(ShopContext);
   const [productData, setProductData] = useState(false);
   const [image, setImage] = useState('');
   const [size, setSize] = useState('');
@@ -96,7 +96,7 @@ useEffect(() => {
                 ))}
               </div>
             </div>
-            <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-100'>ADD TO CART</button>
+            <button onClick = {() => addToCart(productData._id, size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-100'>ADD TO CART</button>
             <hr className='mt-8 sm:w-4/5 '/>
             <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
               <p>100% Original Product.</p>
