7be03e694b4b8448fb1622b4347eca0b27063037
amiksha125 <amikshagajbhiye@gmail.com>
2025-06-21 00:16:07 +0530

    fetch cartItems on cart page

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const Cart = () => {
+  const { products, currency, cartItems } = useContext(ShopContext);
+
+  const [cartData, setcartData] = useState([]); //initialize with empty array
+
+  useEffect(() => {
+     //when cart page is loaded combine product data and cartItems data to create array
+
+     const tempData = [];
+     for(const items in cartItems){
+      for (const item in cartItems[items]){
+        if(cartItems[items][item] > 0){
+             tempData.push({
+              _id: items,
+              size:item,
+              quantity: cartItems[items][item]
+             })
+        }
+      }
+     }
+     console.log(tempData);
+  }, [cartItems])
   return (
     <div>Cart</div>
   )
