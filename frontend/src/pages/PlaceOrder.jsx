c9863fe730f26f59fdaae0b7d925976c2c3f2008
amiksha125 <amikshagajbhiye@gmail.com>
2025-06-24 20:21:53 +0530

    mount carttotal comp on right side of the place-order page and display payment methods

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Title from '../components/Title';
+import CartTotal from '../components/CartTotal';
+import { assets } from '../assets/assets';
 
 const PlaceOrder = () => {
   return (
@@ -26,6 +28,38 @@ const PlaceOrder = () => {
            </div>
             <input type="number" placeholder='Phone' className='border border-gray-300 rounded py-1.5 px-3.5 w-full' />
        </div>
+       
+       {/* ------------------------Right Side-------------------------- */}
+
+       <div className='mt-8'>
+        <div className='mt-8 min-w-80'>
+          <CartTotal />
+        </div>
+
+        <div className='mt-12'>
+          <Title text1={'PAYMENT'} text2={'METHODS'} />
+          {/* ------------------------PAYMENT METHOD SELECTION-------------------------- */}
+          <div className='flex flex-col gap-2 lg:flex-row'>
+             <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+              {/* add dynamic class name as when we select a method then it will get green otherwise transparent */}
+                <p className= {`min-w-3.5 h-3.5 border rounded-full`}></p>
+                <img src= {assets.stripe_logo} className='h-5 mx-4' alt="" />
+             </div>
+
+              <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                <p className= {`min-w-3.5 h-3.5 border rounded-full`}></p>
+                <img src= {assets.razorpay_logo} className='h-5 mx-4' alt="" />
+             </div>
+
+             <div className='flex items-center gap-3 border p-2 px-3 cursor-pointer'>
+                <p className= {`min-w-3.5 h-3.5 border rounded-full`}></p>
+                <p className='text-gray-500 text-sm font-medium mx-4'>CASH ON DELIVERY</p>
+             </div>
+          </div>
+        </div>
+
+       </div>
+       
 
     </div>
   )
