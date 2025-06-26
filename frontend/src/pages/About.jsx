d5c0807d006fa75056b9e0f7f8ba77f9973bb3d0
amiksha125 <amikshagajbhiye@gmail.com>
2025-06-26 21:40:01 +0530

    complete about section with why choose us section and newsletterbox component.

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Title from '../components/Title';
 import { assets } from '../assets/assets';
+import NewsLetterBox from '../components/NewsLetterBox';
 
 const About = () => {
   return (
-    <>
+    <div>
       <div className='text-2xl text-center pt-8 border-t'>
         <Title text1={'ABOUT'} text2={'US'} />
       </div>
@@ -28,7 +29,28 @@ const About = () => {
 
         </div>
       </div>
-    </>
+
+      <div className='text-xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US?'} />
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+           <b>Quality Assurance:</b>
+           <p className='text-gray-600'>We meticulously select and vet each product to ensure it meets our stringent qaulity standards.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+           <b>Convenience:</b>
+           <p className='text-gray-600'>With user-friendly interface and hassle-free ordering process, shopping has been easier.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+           <b>Exceptional Customer Service:</b>
+           <p className='text-gray-600'>Our team of dedicated professional are here to assist you the way, ensuring your satisfaction is our top priority.</p>
+        </div>
+      </div>
+
+      <NewsLetterBox />
+    </div>
   );
 };
 
