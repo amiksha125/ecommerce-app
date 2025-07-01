4d47c1fddd2d5b6185f28582ae84a84094b3e8be
amiksha125 <amikshagajbhiye@gmail.com>
2025-07-01 20:19:46 +0530

    create a product schema and product model

---
+++ backend/models/productModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+//schema is a structure using that we can create data base
+//Define schema
+const productSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    description: {type: String, required: true},
+    price: {type: Number, required: true},
+    image: {type: Array, required: true},
+    category: {type: String, required: true},
+    subCategory: {type: String, required: true},
+    sizes: {type: Array, required: true},
+    bestseller: {type: Boolean},
+    date: {type: Number, required: true}
+})
+
+//create model
+const productModel = mongoose.moodels.product || mongoose.model("product", productSchema)
+//the model will be created once, and if its already available then its used, singleton
+
+export default productModel;
