1528dc99f73770b9b5162135b90ea5de9bcebb3c
amiksha125 <amikshagajbhiye@gmail.com>
2025-07-01 23:13:50 +0530

    create user schema and user mongoose model

---
+++ backend/models/userModel.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+//create schema
+const userSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    cartData: {type: Object, default: {}}, //by default provide empty object
+}, {minimize: false})//so that when user created empty cartData obj is created
+
+// create model 
+
+const userModel = mongoose.model.user || mongoose.model("user", userSchema)
+
+export default userModel;
