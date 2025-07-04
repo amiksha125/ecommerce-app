5613249a1680221c89be1f88e2285df55cca1910
amiksha125 <amikshagajbhiye@gmail.com>
2025-07-04 21:34:45 +0530

    successfully tested posting product details on product/add api , everything is well  but value of price is printed undefined

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,7 +3,25 @@
 //function for adding product
 const addProduct = async (req, res) => {
 // before addProduct function create a multer middleware, so that if we send any file as form data then this will send using multer
+try{
+  
+    const { name, description, price, category, subCategory, sizes, bestseller} = req.body;
 
+    const image1 = req.files.image1[0];
+    const image2 = req.files.image2[0];
+    const image3 = req.files.image3[0];
+    const image4 = req.files.image4[0];
+
+    console.log( name, description, price, category, subCategory, sizes, bestseller );
+    console.log( image1, image2, image3, image4);
+
+    res.json({})
+    
+}catch(err){
+
+  console.log(err);
+   res.json({success: false, message: err.message})
+}
 
      
 }
