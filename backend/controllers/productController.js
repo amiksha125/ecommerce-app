54cdc29720755d519e66488f1230d36b4b6987c1
amiksha125 <amikshagajbhiye@gmail.com>
2025-07-04 21:00:28 +0530

    route setup for productController functions

---
+++ backend/controllers/productController.js
@@ -0,0 +1,23 @@
+//four functions 
+
+//function for adding product
+const addProduct = async (req, res) => {
+     
+}
+
+//function to list products
+const listProduct = async (req, res) => {
+     
+}
+
+//function to remove product
+const removeProduct = async (req, res) => {
+     
+}
+
+//function for single product info
+const singleProduct = async (req, res) => {
+     
+}
+
+export {listProduct, addProduct, removeProduct, singleProduct}
---
+++ backend/routes/productRoute.js
@@ -0,0 +1,16 @@
+import express from 'express'
+
+import {listProduct, addProduct, removeProduct, singleProduct} from '../controllers/productController.js'
+
+const productRouter = express.Router();
+
+productRouter.post('/add', addProduct);
+
+productRouter.post('/remove', removeProduct);
+
+productRouter.post('/single', singleProduct);
+//we have to get the list
+productRouter.get('/list', listProduct);
+
+export default productRouter;
+
