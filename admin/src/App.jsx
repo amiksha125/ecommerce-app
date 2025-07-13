32f900d183c634ceff1adc469b474be4f4c10da3
amiksha125 <amikshagajbhiye@gmail.com>
2025-07-13 23:36:11 +0530

    Login page is created for admin panel and the panel is loaded only after authentication

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,38 +1,41 @@
-import React from 'react'
-import Navbar from './Components/Navbar'
-import Sidebar from './Components/Sidebar'
-import { Routes, Route } from 'react-router-dom'
+import React, { useState } from "react";
+import Navbar from "./Components/Navbar";
+import Sidebar from "./Components/Sidebar";
+import { Routes, Route } from "react-router-dom";
 
-import Add from './Pages/Add'
-import List from './Pages/List'
-import Orders from './Pages/Orders'
+import Add from "./Pages/Add";
+import List from "./Pages/List";
+import Orders from "./Pages/Orders";
+import Login from "./Components/Login";
 
 const App = () => {
+  const [token, setToken] = useState("");
+
   return (
-    <div className='bg-gray-50 min-h-screen'>
-    <>
-    {/* //above tag means a fragment */}
+    <div className="bg-gray-50 min-h-screen">
+      {token === "" ? (
+        <Login />
+      ) : (
+        <>
+          {/* //above tag means a fragment */}
 
-    <Navbar />
-    <hr />
-    <div className='flex w-full'>
-      <Sidebar />
-      {/* Route setup */}
-      <div className='w-[70%] ml-[max(5vw, 25px)] mx-auto my-8 text-gray-600 text-base'>
-        <Routes>
-
-          <Route path='/add' element = {Add}/>
-          <Route path='/list' element = {List}/>
-          <Route path='/orders' element = {Orders}/>
-
-        </Routes>
-
-      </div>
-    </div>      
-    </>
-
+          <Navbar />
+          <hr />
+          <div className="flex w-full">
+            <Sidebar />
+            {/* Route setup */}
+            <div className="w-[70%] ml-[max(5vw, 25px)] mx-auto my-8 text-gray-600 text-base">
+              <Routes>
+                <Route path="/add" element={Add} />
+                <Route path="/list" element={List} />
+                <Route path="/orders" element={Orders} />
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- admin/src/Components/Navbar.jsx
+++ admin/src/Components/Navbar.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import assets from '../assets/assets.js'
+import React from "react";
+import assets from "../assets/assets.js";
 
 const Navbar = () => {
   return (
-    <div className='flex items-center py-2 justify-between'>
-        <img src={assets.logo} alt="Logo" className="w-[max(10%,80px)] ml-9" />
-        <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-xs'>Logout</button>
+    <div className="flex items-center py-2 justify-between">
+      <img src={assets.logo} alt="Logo" className="w-[max(10%,80px)] ml-9" />
+      <button className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-xs">
+        Logout
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
