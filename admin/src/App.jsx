fc832e2da620a19ddc52017d5d00f62e6c779066
amiksha125 <amikshagajbhiye@gmail.com>
2025-07-13 23:17:33 +0530

    Route setup for add, list and orders pages in App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Sidebar from './Components/Sidebar'
+import { Routes, Route } from 'react-router-dom'
+
+import Add from './Pages/Add'
+import List from './Pages/List'
+import Orders from './Pages/Orders'
+
 const App = () => {
   return (
     <div className='bg-gray-50 min-h-screen'>
@@ -11,6 +17,17 @@ const App = () => {
     <hr />
     <div className='flex w-full'>
       <Sidebar />
+      {/* Route setup */}
+      <div className='w-[70%] ml-[max(5vw, 25px)] mx-auto my-8 text-gray-600 text-base'>
+        <Routes>
+
+          <Route path='/add' element = {Add}/>
+          <Route path='/list' element = {List}/>
+          <Route path='/orders' element = {Orders}/>
+
+        </Routes>
+
+      </div>
     </div>      
     </>
 
