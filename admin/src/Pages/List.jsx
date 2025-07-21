b7831037265fe7abbe8e3155822a269573420296
amiksha125 <amikshagajbhiye@gmail.com>
2025-07-22 00:55:22 +0530

    list all the products on the list page and remove product functionality in list

--- admin/src/Pages/List.jsx
+++ admin/src/Pages/List.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { backendUrl } from '../App';
+import { backendUrl, currency } from '../App';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const List = () => {
+const List = ({token}) => {
 
   const [list, setList] = useState([]);
   const fetchList = async () => {
@@ -25,6 +25,25 @@ const List = () => {
 
   }
 
+  const removeProduct = async (id) => {
+    try{
+      const response = await axios.post(backendUrl + '/api/product/remove', {id}, {headers: {token}})
+
+      if(response.data.success){
+        toast.success(response.data.message);
+        //now list will be updated , display new list
+         await fetchList();
+
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch(error){
+      console.log(error);
+      toast.error(error.message);
+
+    }
+  }
+
   useEffect(() => {
     fetchList();
   }, [])
@@ -42,8 +61,21 @@ const List = () => {
           <b>Price</b>
           <b className='text-center'>Action</b>
 
-          {/* Product List */}
+          
         </div>
+        {/* Product List */}
+
+          {
+            list.map((item, index) => (
+              <div className = 'grid grid-cols-[1fr_3fr_1fr] md:grid-cols-[1fr_3fr_1fr_1fr_1fr] items-center gap-2 py-1 px-2 border text-sm'key = {index}>
+                <img className = 'w-12' src= {item.image[0]} alt="" />
+                <p>{item.name }</p>
+                <p>{item.category}</p>
+                <p>{currency}{item.price}</p>
+                <p onClick = {() => removeProduct(item._id)} className='text-right md:text-center cursor-pointer text-lg'>X</p>
+              </div>
+            ))
+          }
      </div>
     </>
   )
