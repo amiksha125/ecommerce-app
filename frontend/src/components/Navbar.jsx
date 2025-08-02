0fb256e11bd877e4840db3c4e73a7692615d1525
amiksha125 <amikshagajbhiye@gmail.com>
2025-08-03 01:30:02 +0530

    disable profile drop down when not logged in and display order page in orders option from drop down

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -53,15 +53,17 @@ const Navbar = () => {
             <img onClick = {() => setShowSearch(true)} src= {assets.search_icon} className='w-5 cursor-pointer' alt="" />
 
             <div className='group relative'>
-                <Link to= '/login'><img src={assets.profile_icon} className='w-5 cursor-pointer' alt="" /> </Link>
-                <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                <img onClick = {(s) => token ? null : navigate('/login') } src={assets.profile_icon} className='w-5 cursor-pointer' alt="" /> 
+                {/* DropDown Menu */}
+                {/* display drop down only when you logged in */}
+                {token && <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                     {/* <p>Parent hovered</p> */}
                     <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                         <p className='cursor-pointer hover:text-black '>My Profile</p>
-                        <p className='cursor-pointer hover:text-black '>Orders</p>
+                        <p onClick = {() => navigate('/orders')} className='cursor-pointer hover:text-black '>Orders</p>
                         <p onClick = {logout} className='cursor-pointer hover:text-black '>Logout</p>
                     </div>
-                </div>
+                </div> }
             </div>
 
             <Link to='/cart' className='relative'>
