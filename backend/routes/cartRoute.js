678cd164afcebddc94bcdc352538a74570ae510a
amiksha125 <amikshagajbhiye@gmail.com>
2025-08-04 00:18:18 +0530

    Add auth middleware to extract userId from token and protect cart routes

--- backend/routes/cartRoute.js
+++ backend/routes/cartRoute.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import { addToCart, updateCart, getUserCartData }  from '../controllers/cartController.js';
+import authUser from '../middleware/auth.js';
+
 
 const cartRouter = express.Router();
 
 //create endpoints with the imported functions
 
-cartRouter.post('/get', getUserCartData);
-cartRouter.post('/add', addToCart);
-cartRouter.post('/update', updateCart);
+cartRouter.post('/get', authUser, getUserCartData);
+cartRouter.post('/add', authUser, addToCart);
+cartRouter.post('/update', authUser, updateCart);
 
 export default cartRouter;
