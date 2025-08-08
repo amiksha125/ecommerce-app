725e81378638dc89d8f1f40e55e445fb8eaacd49
amiksha125 <amikshagajbhiye@gmail.com>
2025-08-08 19:59:43 +0530

    post the cart data into orders section of database through api

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -3,12 +3,12 @@ import Title from '../components/Title';
 import CartTotal from '../components/CartTotal';
 import { assets } from '../assets/assets';
 import { ShopContext } from '../context/ShopContext';
-
-
+import { toast } from 'react-toastify';
+import axios from 'axios';
 
 const PlaceOrder = () => {
 
-   const { navigate, backendUrl, token, cartItems, setCartItems, getCartAmount, deliver_fee, products } = useContext(ShopContext);
+   const { navigate, backendUrl, token, cartItems, setCartItems,  getCartAmount, delivery_fee, products } = useContext(ShopContext);
 
   const [method, setmethod] = useState('cod');
 
@@ -51,7 +51,33 @@ const PlaceOrder = () => {
           }
       }
 
-      console.log(orderItems);
+     // console.log(orderItems);
+
+     let orderData = {
+      address: formData,
+      items: orderItems,
+      amount: getCartAmount() + delivery_fee
+     }
+
+     switch (method) {
+      case 'cod':
+         const response = await axios.post(
+                           backendUrl + '/api/order/place',
+                           orderData,
+                         { headers: { token } }
+                    );
+
+         if(response.data.success){
+            setCartItems({});
+            navigate('/orders');
+         } else {
+            toast.error(response.data.message);
+         }
+         break;
+     
+      default:
+         break;
+     }
       
       
    } catch (error) {
