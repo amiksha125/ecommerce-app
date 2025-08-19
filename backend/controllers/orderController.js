183e704220d06565466664671565077ab013c643
amiksha125 <amikshagajbhiye@gmail.com>
2025-08-19 13:48:21 +0530

    created stripe payment gateway, when user proceed with payment method stripe he is redirected to stripe payment gateway and then again back to our website!

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,15 @@
+// import { currency } from "../../admin/src/App.jsx";
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
+import Stripe from 'stripe'
+
+//global variables
+const currency = 'USD';
+const deliveryCharge = 10
+
+// initialize gateway
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+
 // Placing orders using Cash On Delivery method 
 const placeOrder = async (req, res) => {
 
@@ -16,7 +26,7 @@ const placeOrder = async (req, res) => {
             payment: false,
             date: Date.now()
         }
-        //Save the orders 
+        //Create and Save the orders 
         const newOrder = new orderModel(orderData);
         await newOrder.save();
        //Clear the cart data from user Id 
@@ -32,6 +42,63 @@ const placeOrder = async (req, res) => {
 }
 // Placing orders using Stripe method 
 const placeOrderStripe = async (req, res) => {
+
+    try {
+        
+        const { userId, items, amount, address } = req.body;
+        const { origin } = req.headers; //origin is the frontend url from where u send request
+      
+        const orderData = {
+            userId,
+            items,
+            amount,
+            address,
+            paymentMethod: "Stripe",
+            payment: false,
+            date: Date.now()
+        }
+
+        const newOrder = new orderModel(orderData);
+        await newOrder.save();
+
+        const line_items = items.map( (item) => ({
+            price_data: {
+                currency: currency,
+                product_data: {
+                    name : item.name,
+                },
+                unit_amount: item.price * 100
+            },
+
+            quantity: item.quantity
+        }))
+
+        line_items.push( {
+            price_data: {
+                currency: currency,
+                product_data: {
+                    name : 'Delivery Charges'
+                },
+                unit_amount: deliveryCharge * 100
+            },
+
+            quantity: 1
+        } )
+
+        const session = await stripe.checkout.sessions.create({
+            success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+            line_items,
+            mode: 'payment'
+        })
+
+        res.json({success: true, session_url: session.url})
+
+
+    } catch (error) {
+         console.log(error);
+        res.json({ success: false, message: error.message })
+    }
 
 }
 // Placing orders using Razorpay method 
